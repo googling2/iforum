579b425360b94318a95781b61d8a87d337c6c0d2
kucu-try <baekfive@gmail.com>
2024-05-09 16:56:14 +0900

    bjh

--- models.py
+++ models.py
@@ -1,31 +1,19 @@
 from email.policy import default
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Date, CHAR
 from sqlalchemy.orm import relationship
-from db import Base
+from database import Base
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = 'user'
 
-    user_code = Column(Integer, primary_key=True, autoincrement=True)
+    user_code = Column(Integer, primary_key=True, index=True)
     user_name = Column(String(50), nullable=False)
-    email = Column(String(30), nullable=False)
+    email = Column(String(30), nullable=False, unique=True, index=True)
     profile = Column(Integer)
     joinDate = Column(Date, nullable=False)
     clientId = Column(String(255), nullable=False)
     secretId = Column(String(255), nullable=False)
     accessToken = Column(String(255), nullable=False)
-    refreshToken = Column(String(255), nullable=False)
-    status = Column(CHAR(1), default='N', nullable=False)
+    refreshToken = Column(String(255))
+    status = Column(String(1), nullable=False, default='N', comment='탈퇴시 Y로 영상 올린 것들때문에')
 
-    voice_records = relationship("VoiceRecord", back_populates="user")
-
-class VoiceRecord(Base):
-    __tablename__ = "voice"
-
-    voice_code = Column(Integer, primary_key=True, autoincrement=True)
-    user_code = Column(Integer, ForeignKey('user.user_code'), nullable=False)
-    voice_name = Column(String(255), nullable=False)
-    voice_date = Column(Date, nullable=False)
-    voice_status = Column(CHAR(1), default='N', nullable=False)
-
-    user = relationship("User", back_populates="voice")
